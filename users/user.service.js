0e8df5395971d07be2f17fc46fb054d1a41c90ad
Karthikeyan Jeganathan <kjeganathan@deloitte.de>
2020-07-11 23:47:57 +0200

    Initial commit

---
+++ users/user.service.js
@@ -0,0 +1,39 @@
+﻿// users hardcoded for simplicity
+const users = [
+    {
+        id: 1,
+        username: "test",
+        password: "test",
+        firstName: "Test",
+        lastName: "User",
+    },
+    {
+        id: 2,
+        username: "maxmustermann",
+        password: "test123",
+        firstName: "Max",
+        lastName: "Mustermann",
+    },
+];
+
+module.exports = {
+    authenticate,
+    getAll,
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(
+        (u) => u.username === username && u.password === password
+    );
+    if (user) {
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+}
+
+async function getAll() {
+    return users.map((u) => {
+        const { password, ...userWithoutPassword } = u;
+        return userWithoutPassword;
+    });
+}
---
+++ users/users.controller.js
@@ -0,0 +1,35 @@
+﻿const express = require("express");
+const multiparty = require("multiparty");
+const router = express.Router();
+const userService = require("./user.service");
+
+// routes
+router.post("/authenticate", authenticate);
+router.get("/", getAll);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+    const form = new multiparty.Form();
+    form.parse(req, function (err, fields, files) {
+        const username = fields.username[0];
+        const password = fields.password[0];
+        userService
+            .authenticate({ username, password })
+            .then((user) =>
+                user
+                    ? res.json(user)
+                    : res.status(400).json({
+                          message: "Username or password is incorrect",
+                      })
+            )
+            .catch((err) => next(err));
+    });
+}
+
+function getAll(req, res, next) {
+    userService
+        .getAll()
+        .then((users) => res.json(users))
+        .catch((err) => next(err));
+}
